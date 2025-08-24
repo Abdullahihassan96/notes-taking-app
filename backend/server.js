b09dc0a6e8f4ccb8469e883a9a05532cc953d9c5
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2025-08-25 00:24:12 +0300

    fixed some bugs

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,13 @@ app.get("/api/health", (req, res) => {
 });
 
 // Routes
-app.use("/notes", noteRoutes);
+app.use("/api/notes", noteRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+//in production, no localhost..so we make dynamic
 const baseURL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "api";
 
 export const api = axios.create({
-  baseURL,
+  baseURL: baseURL,
 });

ab4a6d9015dd1d68ba7383a5a3fa02b9cd1fa18a
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2025-08-25 01:58:18 +0300

    frontend api fixed

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //in production, no localhost..so we make dynamic
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "";
 
 export const api = axios.create({
   baseURL: BASE_URL,

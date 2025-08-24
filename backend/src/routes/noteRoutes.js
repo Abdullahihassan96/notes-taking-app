efd02dd7f7b9048e8971914ec30552e8c0612e21
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2025-08-25 01:47:54 +0300

    fixed api issues

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -8,19 +8,19 @@ import {
   deleteNote,
 } from "../controllers/noteController.js";
 
-const router = express.Router();
+const router = Router();
 
-router.get("/", getNotes);
+// router.get("/", getNotes);
 
-router.get("/:id", getNoteById);
+// router.get("/:id", getNoteById);
 
-router.post("/", createNote);
+// router.post("/", createNote);
 
-router.put("/:id", updateNote);
+// router.put("/:id", updateNote);
 
-router.delete("/:id", deleteNote);
+// router.delete("/:id", deleteNote);
 
-// router.route("/").get(getNotes).post(createNote);
-// router.route("/:id").get(getNoteById).put(updateNote).delete(deleteNote);
+router.route("/").get(getNotes).post(createNote);
+router.route("/:id").get(getNoteById).put(updateNote).delete(deleteNote);
 
 export default router;

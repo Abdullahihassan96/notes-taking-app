35e7c05830c2f6e566d2ce0cfe1fb0403d2ee121
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2025-08-24 23:05:12 +0300

    fixed api issue

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-//in production, we make this dynamic i.e no localhost
-const BASE_URL =
+const baseURL =
   import.meta.env.MODE === "development" ? "http://localhost:5000" : "/api";
 
 export const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL,
 });

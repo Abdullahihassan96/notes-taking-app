9cc1050eb23216158d5914e86bd70d35430a7551
Abdullahihassan96 <ibnuhashoy96@gmail.com>
2026-07-23 17:24:54 +0300

    changed title

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,6 +6,8 @@ export async function connectDB() {
     throw new Error("MONGO_URI is not set in .env");
   }
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri, { dbName: process.env.MONGO_DB || "mern_notes" });
+  await mongoose.connect(uri, {
+    dbName: process.env.MONGO_DB || "mern_notes",
+  });
   console.log("✅ MongoDB connected");
 }
